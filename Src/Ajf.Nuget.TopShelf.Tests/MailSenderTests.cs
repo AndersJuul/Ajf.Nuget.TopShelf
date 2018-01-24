27da2eefd4422cde6e748a9490475a62b9a86026
AndersJuul <andersjuulsfirma@gmail.com>
2018-01-24 20:12:06 +0100

    a

---
+++ Src/Ajf.Nuget.TopShelf.Tests/MailSenderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Net;
+using NUnit.Framework;
+
+namespace Ajf.Nuget.Logging.Tests
+{
+    [TestFixture]
+    public class MailSenderTests
+    {
+        [Test]
+        public void TestThatMailCanBeSend()
+        {
+            //File.WriteAllText("dummy.txt","Lazy hound, quick fox etc");
+
+            //var mailSender = new MailSender();
+            //var httpStatusCode = mailSender.SendMailAsync("andersjuulsfirma@gmail.com", "andersjuulsfirma@gmail.com",
+            //    "andersjuulsfirma@gmail.com", "TestEmail: Subject", "<html><html>", new string[] {"dummy.txt" }).Result;
+
+            //Assert.AreEqual(HttpStatusCode.Accepted, httpStatusCode);
+        }
+    }
+}
---
+++ Src/Ajf.Nuget.TopShelf/BaseWorker.cs
@@ -0,0 +1,8 @@
+namespace Ajf.Nuget.TopShelf
+{
+    public abstract class BaseWorker
+    {
+        public abstract void Start();
+        public abstract void Stop();
+    }
+}
